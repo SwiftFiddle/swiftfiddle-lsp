efe83ee8ba4ee13dddbcd32236ccb0e8cc6e1cda
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2021-12-19 03:27:59 +0900

    test

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -77,8 +77,8 @@ func routes(_ app: Application) throws {
             date = Date()
             try fileManager.createDirectory(atPath: workspacePath, withIntermediateDirectories: true, attributes: nil)
             try copyWorkspace(
-                atPath: "\(app.directory.resourcesDirectory)ProjectTemplate/",
-                toPath: "\(workspacePath)/"
+                atPath: "\(app.directory.resourcesDirectory)ProjectTemplate",
+                toPath: workspacePath
             )
             req.logger.notice("\(Date().timeIntervalSince1970 - date.timeIntervalSince1970) COPY")
         } catch {
@@ -264,21 +264,10 @@ func routes(_ app: Application) throws {
 }
 
 private func copyWorkspace(atPath sourcePath: String, toPath destPath: String) throws {
-    let exec = Process()
-
-    exec.executableURL = URL(fileURLWithPath: "/usr/bin/rsync")
-    exec.arguments = [
-        "-a",
-        "--delete",
-        #"--include="Package.*""#,
-        #"--include="Sources/""#,
-        #"--include="Sources/**""#,
-        #"--include=".build/""#,
-        #"--include=".build/debug.yaml""#,
-        #"--exclude="*""#,
-        #"--exclude="*/""#,
-        sourcePath, destPath
-    ]
-    exec.launch()
-    exec.waitUntilExit()
+    let fileManager = FileManager()
+    try fileManager.createDirectory(atPath: "\(sourcePath)/.build/", withIntermediateDirectories: true, attributes: nil)
+    try fileManager.copyItem(atPath: "\(sourcePath)/.build/debug.yaml", toPath: destPath)
+    try fileManager.copyItem(atPath: "\(sourcePath)/Package.resolved", toPath: destPath)
+    try fileManager.copyItem(atPath: "\(sourcePath)/Package.swift", toPath: destPath)
+    try fileManager.copyItem(atPath: "\(sourcePath)/Sources", toPath: destPath)
 }
