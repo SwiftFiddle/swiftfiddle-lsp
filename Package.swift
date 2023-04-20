9072a116e3d8a68b7da8f5f625455418443df839
Kishikawa Katsumi <kishikawakatsumI@mac.com>
2023-04-20 09:35:15 +0900

    Fix deployment target

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "swiftfiddle-lsp",
     platforms: [
-        .macOS("10.15.4")
+        .macOS(.v12)
     ],
     dependencies: [
         .package(url: "https://github.com/vapor/vapor.git", from: "4.76.0"),
