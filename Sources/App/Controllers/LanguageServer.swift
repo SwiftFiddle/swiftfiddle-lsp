ae8d814b8afd8edc16a7e2067574fb1910e1fc2f
Kishikawa Katsumi <kishikawakatsumi@mac.com>
2024-11-05 20:42:35 +0900

    Remove --log-level option

--- Sources/App/Controllers/LanguageServer.swift
+++ Sources/App/Controllers/LanguageServer.swift
@@ -43,7 +43,6 @@ final class LanguageServer {
 
         serverProcess.executableURL = URL(fileURLWithPath: launchPath)
         serverProcess.arguments = [
-            "--log-level", "error",
             "-Xswiftc", "-enable-bare-slash-regex",
         ]
 
